c0d59f841c2a1839ea365db0edef2e42eb09347a
Melkish <sannamelke93@gmail.com>
2015-09-20 15:04:24 +0200

    level loading working

--- Assets/Sidescroll/Scripts/Exit.cs
+++ Assets/Sidescroll/Scripts/Exit.cs
@@ -9,12 +9,12 @@ public class Exit : MonoBehaviour {
 		if(other.CompareTag("Player")) {
 
 			other.gameObject.SetActive(false);
-			particle1.SetActive(true);
-			particle2.SetActive(true);
+			//particle1.SetActive(true);
+			//particle2.SetActive(true);
 			// particle1 och particle2 borde sättas aktiva här.
 
 			yield return new WaitForSeconds(2f);
-
+			Application.LoadLevel(Application.loadedLevel+1);
 			// Här bör vi ladda en ny scen.
 		}
 	}
